981be87d4e7e68a85011875361a6210b026ce7fe
기영 정 <jkyoung710@naver.com>
2023-03-21 17:09:10 +0900

    초기 커밋.

---
+++ JeongKiYoung_ex1/MainApp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace JeongKiYoung_ex1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+            }
+
+            Console.Write("정수를 입력하세요 : ");
+
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            
+
+            for (int a = 1; a <= number; a++)
+            {
+                for (int b = 1; b <= a; b++)
+                {
+
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ JeongKiYoung_ex2/MainApp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace JeongKiYoung_ex2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 5; 1 <= i; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+            }
+
+            Console.Write("정수를 입력하세요 : ");
+
+
+            string input1 = Console.ReadLine();
+            int number1 = Convert.ToInt32(input1);
+
+
+            for (int a = number1; 1 <= a; a--)
+            {
+                for (int b = 1; b <= a; b++)
+                {
+
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+            }
+
+
+
+
+        }
+    }
+}
